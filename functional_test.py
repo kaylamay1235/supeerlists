fada2bb2be459a6d3f61af60f16e280af6ca684f
YOUR NAME <YOUR@EMAIL.com>
2021-09-30 08:37:16 -0500

    Add app for lists , with deliberately failing unit test

---
+++ functional_test.py
@@ -0,0 +1,7 @@
+from selenium import webdriver
+from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
+
+binary = FirefoxBinary('path/to/installed firefox binary')
+browser = webdriver.Firefox(firefox_binary=binary)
+browser.get ('http://localhost:8000')
+assert 'Django' in browser.title
